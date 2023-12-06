15fceee94254fe7f4b6f44aad076c06dd54c5568
xxflyxx <xxflyxx@163.com>
2023-12-07 06:49:14 +0800

    save

---
+++ Plugins/UIExtension/Source/UIExtension.Build.cs
@@ -0,0 +1,35 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class UIExtension : ModuleRules
+{
+	public UIExtension(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"CoreUObject",
+				"Engine",
+				"SlateCore",
+				"Slate",
+				"UMG",
+				"CommonUI",
+				"GameplayTags"
+			}
+		);
+
+        PublicIncludePathModuleNames.AddRange(
+            new string[] {
+            }
+        );
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				// ... add private dependencies that you statically link with here ...
+			}
+		);
+	}
+}
