f20752f63c74abfa7fbbb267609ea4eb0e11fcf1
xxflyxx <xxflyxx@163.com>
2023-12-11 03:32:50 +0800

    fan

---
+++ Plugins/GameFeatures/DreamEpibolyCore/Source/DreamEpibolyCore/Private/Fan/FanComponent.cpp
@@ -0,0 +1,89 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FanComponent.h"
+#include "Components/BoxComponent.h"
+#include "GameFramework/Character.h"
+
+
+FName UFanComponent::IgnoreFan = FName("Ignore_Fan");
+
+UFanComponent::UFanComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.TickInterval = 0.1f;
+}
+
+void UFanComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	SetComponentTickEnabled(false);
+
+	TArray<UBoxComponent*> OverlapComponents;
+	GetOwner()->GetComponents<UBoxComponent>(OverlapComponents);
+	for (UBoxComponent* OverlapComponent : OverlapComponents)
+	{
+		if (OverlapComponent->ComponentHasTag(IgnoreFan))
+			continue;
+		
+		if (OverlapComponent->GetCollisionEnabled() == ECollisionEnabled::QueryOnly)
+		{
+			OverlapComponent->OnComponentBeginOverlap.AddDynamic(this, &ThisClass::OnOverlapChange);
+		}
+	}
+}
+
+void UFanComponent::OnOverlapChange(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	const bool bIsTickEnable = IsComponentTickEnabled();
+	TArray<UBoxComponent*> OverlapComponents;
+	GetOwner()->GetComponents<UBoxComponent>(OverlapComponents);
+	for (UBoxComponent* OverlapComponent : OverlapComponents)
+	{
+		if (OverlapComponent->ComponentHasTag(IgnoreFan))
+			continue;
+		
+		if (OverlapComponent->GetOverlapInfos().Num() > 0)
+		{
+			if (!bIsTickEnable)
+				SetComponentTickEnabled(true);
+			return;
+		}
+	}
+
+	if (bIsTickEnable)
+		SetComponentTickEnabled(false);
+}
+
+
+void UFanComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	TArray<UBoxComponent*> OverlapComponents;
+	GetOwner()->GetComponents<UBoxComponent>(OverlapComponents);
+	for (UBoxComponent* OverlapComponent : OverlapComponents)
+	{
+		if (OverlapComponent->ComponentHasTag(IgnoreFan))
+			continue;
+		
+		TArray<AActor*> OverlapActors;
+		OverlapComponent->GetOverlappingActors(OverlapActors);
+		for (AActor* OverlapActor : OverlapActors)
+		{
+			if (ACharacter* Character = Cast<ACharacter>(OverlapActor))
+			{
+				const float Distance = (Character->GetActorLocation()-GetOwner()->GetActorLocation()).Size();
+				
+				const float Amplitude = FMath::GetMappedRangeValueClamped(
+					FVector2D(0.f, OverlapComponent->GetScaledBoxExtent().Z),
+					FVector2D(Strength, 0.f), Distance);
+
+				FVector LaunchVelocity = OverlapComponent->GetUpVector()*Amplitude + FVector(0.f, 0.f, 20.f);
+				Character->LaunchCharacter(LaunchVelocity, false, true);
+			}
+		}
+	}
+}
+
---
+++ Plugins/GameFeatures/DreamEpibolyCore/Source/DreamEpibolyCore/Private/Fan/FanComponent.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "FanComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class DREAMEPIBOLYCORE_API UFanComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UFanComponent();
+
+protected:
+	virtual void BeginPlay() override;
+	UFUNCTION()
+	void OnOverlapChange(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	
+	static FName IgnoreFan;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Strength = 800.f;
+};
